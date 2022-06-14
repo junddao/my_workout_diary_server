47a829ca62beb1fca0e9f8f40c1bc068017fd95f
junddao <y2hunter@gmail.com>
2022-06-14 15:18:47 +0900

    :art: add kakao login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { InSignUpDto } from './dto/in_sign_up.dto';
-import { AuthService, KakaoService } from './auth.service';
+import { AuthService } from './auth.service';
 import {
   Body,
   Controller,
@@ -7,6 +7,7 @@ import {
   Param,
   Patch,
   Post,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { User } from './schemas/user.schema';
@@ -14,13 +15,11 @@ import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './get-user.decorator';
 import { InSignInDto } from './dto/in_sign_in.dto';
 import { InUpdateUserDto } from './dto/in_update_user.dto';
+import { InSignInKakaoDto } from './dto/in_sign_in_kakao.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly kakaoService: KakaoService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Get('/me')
   @UseGuards(AuthGuard())
@@ -55,5 +54,12 @@ export class AuthController {
     @Body() inUpdateUserDto: InUpdateUserDto,
   ): Promise<User> {
     return this.authService.updateUser(id, inUpdateUserDto);
+  }
+
+  @Post('/kakao')
+  signInKakao(
+    @Body() inSignInKakao: InSignInKakaoDto,
+  ): Promise<{ token: string }> {
+    return this.authService.signInKakao(inSignInKakao);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,40 @@
+import { InSignInKakaoDto } from './dto/in_sign_in_kakao.dto';
 import { InSignUpDto } from './dto/in_sign_up.dto';
 import { UsersRepository } from './users.repository';
-import { HttpService, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { User } from './schemas/user.schema';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcryptjs';
 import { InUpdateUserDto } from './dto/in_update_user.dto';
-import qs from 'qs';
 import { InSignInDto } from './dto/in_sign_in.dto';
+import * as firebase from 'firebase-admin';
+import * as serviceAccount from './serviceAccountKey.json';
+
+const firebase_params = {
+  type: serviceAccount.type,
+  projectId: serviceAccount.project_id,
+  privateKeyId: serviceAccount.private_key_id,
+  privateKey: serviceAccount.private_key,
+  clientEmail: serviceAccount.client_email,
+  clientId: serviceAccount.client_id,
+  authUri: serviceAccount.auth_uri,
+  tokenUri: serviceAccount.token_uri,
+  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
+  clientC509CertUrl: serviceAccount.client_x509_cert_url,
+};
 
 @Injectable()
 export class AuthService {
+  private admin: any;
+
   constructor(
     private readonly usersRepository: UsersRepository,
     private jwtService: JwtService,
-  ) {}
+  ) {
+    this.admin = firebase.initializeApp({
+      credential: firebase.credential.cert(firebase_params),
+      // databaseURL: 'https://my-workout-diary-d1093.firebaseio.com',
+    });
+  }
 
   async getMe(user: User): Promise<User> {
     const { _id } = user;
@@ -43,6 +64,29 @@ export class AuthService {
     //   throw new UnauthorizedException('login failed');
     // }
   }
+  async signInKakao(
+    inSignInKakaoDto: InSignInKakaoDto,
+  ): Promise<{ token: string }> {
+    console.log('enter!!');
+
+    const uid = inSignInKakaoDto.uid;
+    const updateParams = {
+      email: inSignInKakaoDto.email,
+      photoUrl: inSignInKakaoDto.photoUrl,
+      name: inSignInKakaoDto.name,
+    };
+
+    try {
+      await this.admin.auth().updateUser(uid, updateParams);
+    } catch (e) {
+      updateParams['uid'] = uid;
+      await this.admin.auth().createUser(updateParams);
+    }
+
+    const token = await this.admin.auth().createCustomToken(uid);
+
+    return { token };
+  }
 
   async updateUser(
     id: string,
@@ -52,51 +96,51 @@ export class AuthService {
   }
 }
 
-@Injectable()
-export class KakaoService {
-  private http: HttpService;
-  constructor() {
-    this.http = new HttpService();
-  }
-  async getUserInfo(access_token: string) {
-    //console.log(access_token);
-    return await fetch('https://kapi.kakao.com/v2/user/me', {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    }).then((res) => res.json());
-  }
+// @Injectable()
+// export class KakaoService {
+//   private http: HttpService;
+//   constructor() {
+//     this.http = new HttpService();
+//   }
+//   async getUserInfo(access_token: string) {
+//     //console.log(access_token);
+//     return await fetch('https://kapi.kakao.com/v2/user/me', {
+//       method: 'GET',
+//       headers: {
+//         Authorization: `Bearer ${access_token}`,
+//       },
+//     }).then((res) => res.json());
+//   }
 
-  // async kakaoLogin(url: string, headers: any): Promise<any> {
-  //   return await this.http.post(url, '', { headers }).toPromise();
-  // }
+//   // async kakaoLogin(url: string, headers: any): Promise<any> {
+//   //   return await this.http.post(url, '', { headers }).toPromise();
+//   // }
 
-  async getToken(
-    token: string,
-    client_id: string,
-    redirect_uri: string,
-  ): Promise<any> {
-    return await fetch('https://kauth.kakao.com/oauth/token', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: qs.stringify({
-        grant_type: 'authorization_code',
-        client_id: client_id,
-        redirect_uri: redirect_uri,
-        code: token,
-      }),
-    }).then((res) => res.json());
-  }
+//   async getToken(
+//     token: string,
+//     client_id: string,
+//     redirect_uri: string,
+//   ): Promise<any> {
+//     return await fetch('https://kauth.kakao.com/oauth/token', {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+//       body: qs.stringify({
+//         grant_type: 'authorization_code',
+//         client_id: client_id,
+//         redirect_uri: redirect_uri,
+//         code: token,
+//       }),
+//     }).then((res) => res.json());
+//   }
 
-  async logoutToken(access_token: string) {
-    const respone = await fetch('https://kapi.kakao.com/v1/user/unlink', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: `Bearer ${access_token}`,
-      },
-    }).then((res) => res.json());
-    console.log(respone);
-  }
-}
+//   async logoutToken(access_token: string) {
+//     const respone = await fetch('https://kapi.kakao.com/v1/user/unlink', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded',
+//         Authorization: `Bearer ${access_token}`,
+//       },
+//     }).then((res) => res.json());
+//     console.log(respone);
+//   }
+// }
